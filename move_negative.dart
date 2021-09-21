7fa9fe2107e73159de43d3a3c70154e7b865fdd8
Suhas157 <49067655+Suhas157@users.noreply.github.com>
2021-09-22 01:37:25 +0530

    4. Move the negative elements

---
+++ move_negative.dart
@@ -0,0 +1,8 @@
+// 4. Move All the negative elements to one side of the array
+void main() {
+  List<int> arr = [-7, 9, -5, 0, -3, 4, 5, -6, 7, 1];
+  print("List is :$arr");
+  arr.sort(); 
+
+  print("Negative to Positive elements :$arr");
+}
