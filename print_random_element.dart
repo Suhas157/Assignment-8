a193cebf26917a2184e690f4e48e1f6ded00965c
Suhas157 <49067655+Suhas157@users.noreply.github.com>
2021-09-21 23:23:35 +0530

    Create print_random_element.dart

---
+++ print_random_element.dart
@@ -0,0 +1,9 @@
+// 1. Declare Array/List Of Int Pick and Print Random Element from same Array
+void main() {
+  
+  List arr = [1,2,3,4,5,6,7,8,9,0];
+  print("List is :$arr");
+  arr.shuffle();  // Shuffles the elements of this list randomly.
+  print ("Random Element :${arr[0]}");  // print 0 index element
+  
+}
